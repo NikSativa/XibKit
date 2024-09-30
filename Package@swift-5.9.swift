684dcaa347868b5e96bbfba65123ed3af07f07ef
Mykyta Konopelko <nik.sativa@gmail.com>
2024-09-30 19:33:47 +0200

    fix Swift 5.9 + Package.swift

---
+++ Package@swift-5.9.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:6.9
+// swiftformat:disable all
+import PackageDescription
+
+let package = Package(
+    name: "XibKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "XibKit", targets: ["XibKit"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/NikSativa/SpryKit.git", .upToNextMajor(from: "3.0.0"))
+    ],
+    targets: [
+        .target(name: "XibKit",
+                dependencies: [
+                ],
+                path: "Source",
+                resources: [
+                    .process("PrivacyInfo.xcprivacy")
+                ]),
+        .testTarget(name: "XibKitTests",
+                    dependencies: [
+                        "SpryKit",
+                        "XibKit"
+                    ],
+                    path: "Tests")
+    ]
+)
