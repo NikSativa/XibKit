bf457e05fa40b3a2f0826681d117bd087c15613c
Mykyta Konopelko <nik.sativa@gmail.com>
2026-06-12 11:16:59 +0300

    drop Swift 5.10 fallback, inline Sendable conformances

--- Source/syncMain.swift
+++ Source/syncMain.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-#if swift(>=6.0)
 @inline(__always)
 internal func syncMain(_ block: @MainActor () -> Void) {
     if Thread.isMainThread {
@@ -9,13 +8,3 @@ internal func syncMain(_ block: @MainActor () -> Void) {
         DispatchQueue.main.sync(execute: block)
     }
 }
-#else
-@inline(__always)
-internal func syncMain(_ block: () -> Void) {
-    if Thread.isMainThread {
-        block()
-    } else {
-        DispatchQueue.main.sync(execute: block)
-    }
-}
-#endif
