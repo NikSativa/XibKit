5ce53bb725d018d30e242de11b2efae90256373d
Mykyta Konopelko <nik.sativa@gmail.com>
2026-06-15 21:52:07 +0300

    lower iOS minimum to 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "XibKit",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "XibKit", targets: ["XibKit"])
